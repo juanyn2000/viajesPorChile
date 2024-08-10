383dbc8e9ddfe7dd587ca4cdccffbe8af5823006
juanyn2000 <juanyn1984@gmail.com>
2024-08-09 23:50:54 -0400

    actualizacion viacjes chile 2

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+
+
+//navbar
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("navbar");
+  if (window.scrollY > 250) {
+    navbar.classList.add("bg-black");
+  } else {
+    navbar.classList.remove("bg-black");
+  }
+});
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+//alert
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('¡Gracias! tu mensaje fue enviado correctamente', 'success')
+  })
+}
